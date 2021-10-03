e67ea9cff35454861b106a5e28e50adcd6d7d133
Victoria Lisouskaya <victoria.lisouskaya@gmail.com>
2021-10-03 20:02:35 +0300

    added chrome_options to conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,12 @@ import pytest
 
 @pytest.fixture
 def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(60)
-    yield browser
-    browser.quit()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+    yield driver
+    driver.quit()
